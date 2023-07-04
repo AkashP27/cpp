fa25c3c26c7009ced8297bf8c12b61e7ecc86ded
AkashP27 <akashphayade27@gmail.com>
2023-07-04 16:56:06 +0530

    levelorder  traversal

--- Trees/binaryTree.cpp
+++ Trees/binaryTree.cpp
@@ -36,6 +36,36 @@ Node *createTree(Node *root)
     return root;
 }
 
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+
+        if (temp == NULL)
+        {
+            cout << endl;
+            if (!q.empty())
+                q.push(NULL);
+        }
+        else
+        {
+            cout << temp->data << " ";
+
+            if (temp->left)
+                q.push(temp->left);
+
+            if (temp->right)
+                q.push(temp->right);
+        }
+    }
+}
+
 void inOrder(Node *root)
 {
     if (root == NULL)
@@ -71,6 +101,10 @@ int main()
 
     Node *root = NULL;
     root = createTree(root);
+
+    // 1 3 7 -1 -1 11 -1 -1 5 17 -1 -1 -1
+    cout << "Level order traversal: " << endl;
+    levelOrderTraversal(root);
 
     cout << endl
          << "Inorder traversal: ";
