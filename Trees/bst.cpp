464daf3d7c2a4518a00993e949625bc897362f9e
AkashP27 <akashphayade27@gmail.com>
2023-07-11 11:15:23 +0530

    creating bst

---
+++ Trees/bst.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *insertIntoBST(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        root = new Node(data);
+        return root;
+    }
+
+    if (data < root->data)
+        root->left = insertIntoBST(root->left, data);
+    else
+        root->right = insertIntoBST(root->right, data);
+    return root;
+}
+
+void takeInput(Node *&root)
+{
+    int data;
+    cin >> data;
+
+    while (data != -1)
+    {
+        root = insertIntoBST(root, data);
+        cin >> data;
+    }
+}
+
+int main()
+{
+    Node *root = NULL;
+
+    cout << "Enter data to create BST:" << endl;
+    takeInput(root);
+
+    return 0;
+}
